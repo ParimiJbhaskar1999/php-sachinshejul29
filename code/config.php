5bffa05f564c7bfe740b808e981c02ca2ba4b509
Sachin Shejul <sachin09shejul@gmail.com>
2021-09-24 13:24:25 +0530

    Files updated with sample data

--- code/config.php
+++ code/config.php
@@ -2,8 +2,8 @@
 
 // Configuration keys from SendGrid account
 
-define('API_KEY', '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');
-define('from_mail', 'sachin09shejul@gmail.com');
-define('from_name', 'Sachin Shejul');
+define('API_KEY', 'Sample key');
+define('from_mail', 'sampleid@gmail.com');
+define('from_name', 'Sample name');
 
 ?>
